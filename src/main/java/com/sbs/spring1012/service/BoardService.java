a8e64934f6ccf9c19f3fb5f96babce5caad04023
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-11-14 00:25:16 +0900

    게시글 등록 (미완)

--- src/main/java/com/sbs/spring1012/service/BoardService.java
+++ src/main/java/com/sbs/spring1012/service/BoardService.java
@@ -1,36 +1,59 @@
 package com.sbs.spring1012.service;
 
 import com.sbs.spring1012.dto.BoardReqDto;
-import com.sbs.spring1012.dto.BoardResDto;
 import com.sbs.spring1012.entity.Board;
+import com.sbs.spring1012.entity.Category;
 import com.sbs.spring1012.entity.Member;
 import com.sbs.spring1012.repository.BoardRepository;
+import com.sbs.spring1012.repository.CategoryRepository;
 import com.sbs.spring1012.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j //로그 메시지 출력
 @Service  //스프링 빈 컨테이너에 등록
 @RequiredArgsConstructor//생성자를 통해서 의존성 주입을 받기위한 어노테이션
-@Transactional
 public class BoardService {
     private final BoardRepository boardRepository;
     private final MemberRepository memberRepository;
-    //게시글 등럭
-//    public Boolean saveBoard(BoardReqDto boardReqDto,Long id){
-//        try{
-//            Member member = memberRepository.findById(id)
-//        }
-//    }
+    private final CategoryRepository categoryRepository;
 
-    //게시글 -> Entity
-//    public Board coverBoardEntity(BoardReqDto boardReqDto){
-//        Board board = new Board();
-//
-//    }
+    public boolean boardInsert(BoardReqDto boardReqDto,String email) {
+        try {
+            Member member = memberRepository.findByEmail(email).orElseThrow(
+                            () -> new RuntimeException("존재하지않는 회원입니다."));
+            System.out.println("멤버 있음");
+            Category category = categoryRepository.findByCategoryName(boardReqDto.getCategoryName()).orElseThrow(
+                    ()-> new RuntimeException("존재하지않는 카테고리입니다."));
+            System.out.println("카테고리 있음");
+            Board board = new Board();
+
+            board.setMember(member);
+            System.out.println("member저장!");
+            board.setCategory(category);
+            System.out.println("cate저장!");
+            board.setTitle(boardReqDto.getTitle());
+            System.out.println("title 저장");
+            board.setContent(boardReqDto.getContent());
+            System.out.println("content 저장!");
+            board.setBoardType(board.getBoardType());
+            System.out.println("type저장!");
+            board.setImg(board.getImg());
+            System.out.println("img저장!");
+
+            System.out.println(board.getTitle());
+            boardRepository.save(board);
+
+            System.out.println("저장!");
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("에러떳다...");
+            return false;
+        }
+    }
 
 }
 
