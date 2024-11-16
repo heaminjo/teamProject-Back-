9bffd6413e79492bd383c3b584ba12bd3b622542
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-11-17 00:15:46 +0900

    기능 추가

--- src/main/java/com/sbs/spring1012/controller/BoardController.java
+++ src/main/java/com/sbs/spring1012/controller/BoardController.java
@@ -2,11 +2,14 @@ package com.sbs.spring1012.controller;
 
 
 import com.sbs.spring1012.dto.BoardReqDto;
+import com.sbs.spring1012.dto.BoardResDto;
 import com.sbs.spring1012.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController  //Rest API에 대한 요청과 응답처리를 위한 어노테이션
 @Slf4j
@@ -22,9 +25,41 @@ public class BoardController {
         return ResponseEntity.ok(isTrue);
     }
     //게시글 수정
-    @PostMapping("/modify")
-    public ResponseEntity<Boolean> boardModify(@RequestBody BoardReqDto boardReqDto){
-        Boolean isTrue = boardService.boardModify(boardReqDto);
+    @PostMapping("/modify/{id}")
+    public ResponseEntity<Boolean> boardModify(@RequestBody BoardReqDto boardReqDto,@PathVariable Long id){
+        Boolean isTrue = boardService.boardModify(boardReqDto,id);
         return ResponseEntity.ok(isTrue);
     }
+
+    //게시글 삭제
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<Boolean> boardDelete(@PathVariable Long id){
+        Boolean isTrue = boardService.boardDelete(id);
+        return ResponseEntity.ok(isTrue);
+    }
+
+    //게시글 상세조회
+    @PostMapping("/detail/{id}")
+    public ResponseEntity<BoardResDto> boardDetail(@PathVariable Long id){
+        BoardResDto boardResDto = boardService.boardDetail(id);
+        return ResponseEntity.ok(boardResDto);
+    }
+
+    //게시글 전제조회
+    @PostMapping("/list")
+    public ResponseEntity<List<BoardResDto>> boardAll(){
+        List<BoardResDto> list = boardService.getBoardList();
+        return ResponseEntity.ok(list);
+    }
+
+    //조회수
+    @GetMapping("detail/view/{id}")
+    public ResponseEntity<Boolean> boardView(@PathVariable Long id){
+        Boolean isTrue = boardService.viewCount(id);
+        return ResponseEntity.ok(isTrue);
+    }
+
+    //좋아요
+
 }
+
