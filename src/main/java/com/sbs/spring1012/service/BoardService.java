1da35a1bc1a97d0f6016eb0c9face45fb73441eb
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-11-17 00:16:23 +0900

    Add files via upload

--- src/main/java/com/sbs/spring1012/service/BoardService.java
+++ src/main/java/com/sbs/spring1012/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.sbs.spring1012.service;
 
 import com.sbs.spring1012.dto.BoardReqDto;
+import com.sbs.spring1012.dto.BoardResDto;
 import com.sbs.spring1012.entity.Board;
 import com.sbs.spring1012.entity.Category;
 import com.sbs.spring1012.entity.Member;
@@ -12,6 +13,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.xml.stream.Location;
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j //로그 메시지 출력
 @Service  //스프링 빈 컨테이너에 등록
 @RequiredArgsConstructor//생성자를 통해서 의존성 주입을 받기위한 어노테이션
@@ -22,13 +27,11 @@ public class BoardService {
 
     public boolean boardInsert(BoardReqDto boardReqDto,String email) {
         try {
+            Board board = new Board();
             Member member = memberRepository.findByEmail(email).orElseThrow(
                             () -> new RuntimeException("존재하지않는 회원입니다."));
-            System.out.println("멤버 있음");
             Category category = categoryRepository.findByCategoryName(boardReqDto.getCategoryName()).orElseThrow(
                     ()-> new RuntimeException("존재하지않는 카테고리입니다."));
-            System.out.println("카테고리 있음");
-            Board board = new Board();
 
             board.setMember(member);
             board.setCategory(category);
@@ -39,29 +42,26 @@ public class BoardService {
 
             System.out.println(board.getTitle());
             boardRepository.save(board);
+
             return true;
 
         } catch (Exception e) {
-            System.out.println("에러떳다...");
             return false;
         }
     }
- public boolean boardModify(BoardReqDto boardReqDto){
+    //수정
+    public boolean boardModify(BoardReqDto boardReqDto,Long id){
         try{
-            System.out.println("수정한다.");
-            Board board = boardRepository.findById(boardReqDto.getId()).orElseThrow(
+            //게시글 id를 받아와 수정할 게시글 불러오기
+            Board board = boardRepository.findById(id).orElseThrow(
                     () -> new RuntimeException("해당 게시글이 존재하지 않습니다."));
-            System.out.println("게시물 있음.");
+            System.out.println("게시글 찾음");
 
             Category category = categoryRepository.findByCategoryName(boardReqDto.getCategoryName()).orElseThrow(
-                    ()-> new RuntimeException("존재하지않는 카테고리입니다."));
-            System.out.println("카테고리 있음.");
+                    () -> new RuntimeException("해당 카테고리가 존재하지 않습니다."));
 
             // 값이 null이 아니면 업데이트, null이면 기존 값 유지
-            System.out.println(category.getCategoryName());
-            if (boardReqDto.getCategoryName() != null) {
-                board.setCategory(category);
-            }
+
             if (boardReqDto.getCategoryName() != null) {
                 board.setCategory(category);
             }
@@ -82,6 +82,51 @@ public class BoardService {
             boardRepository.save(board);
             return true;
         } catch (Exception e) {
+            return false;
+        }
+    }
+
+    //게시물 삭제
+    public boolean boardDelete(Long id){
+        try {
+            Board board = boardRepository.findById(id).orElseThrow(
+                    () -> new RuntimeException("게시물이 존재하지않습니다."));
+
+            boardRepository.delete(board);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+    //게시물 상세 조회
+    public BoardResDto boardDetail(Long id) {
+        Board board = boardRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("존재하지않는 게시글입니다."));
+
+        return BoardResDto.of(board);
+    }
+    //게시글 전제 조회
+    public List<BoardResDto> getBoardList(){
+        List<Board> boards= boardRepository.findAll();
+        List<BoardResDto> list = new ArrayList<>();
+
+        for(Board board : boards){
+            list.add(BoardResDto.of(board));
+        }
+        return list;
+    }
+
+    //조회수
+    public boolean viewCount(Long id){
+        try{
+            Board board = boardRepository.findById(id).orElseThrow(
+                    ()-> new RuntimeException("존재하지않는 게시글 입니다.")
+            );
+
+            board.setViews((board.getViews())+1);
+            boardRepository.save(board);
+            return true;
+        }catch (Exception e){
             return false;
         }
     }
