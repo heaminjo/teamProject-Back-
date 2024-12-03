bb6bf1f020a8d867f8c4dc9d1c589375170c25fc
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-12-03 23:24:03 +0900

    Add files via upload

--- src/main/java/com/sbs/spring1012/repository/GreateRepository.java
+++ src/main/java/com/sbs/spring1012/repository/GreateRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface GreateRepository extends JpaRepository<Great,Long> {
-    Optional<Great> findByBoardAndMember(Board board, Member member);
+    Boolean existsByBoardAndMember(Board board, Member member);
+    Optional<Great> findByBoardAndMember(Board board ,Member member);
 }
